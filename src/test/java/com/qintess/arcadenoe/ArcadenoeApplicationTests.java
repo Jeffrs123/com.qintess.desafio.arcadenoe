a6ba3aa4953c8cf25086a6f6423ecf05b7d7156c
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-15 11:10:34 -0300

    Comeco projeto

---
+++ src/test/java/com/qintess/arcadenoe/ArcadenoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.arcadenoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadenoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
