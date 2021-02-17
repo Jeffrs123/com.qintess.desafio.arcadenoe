76327fd3c2af68e480c351bbf457f4bbfe0ca1e4
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-17 12:39:21 -0300

    controller, service e exception - PetNotFound - Método ListAll e ListById

--- src/main/java/com/qintess/arcadenoe/controller/PetController.java
+++ src/main/java/com/qintess/arcadenoe/controller/PetController.java
@@ -1,10 +1,14 @@
 package com.qintess.arcadenoe.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.qintess.arcadenoe.dto.request.PetDTO;
 import com.qintess.arcadenoe.dto.response.MessageResponseDTO;
 import com.qintess.arcadenoe.entity.Pet;
+import com.qintess.arcadenoe.exception.PetNotFoundException;
 import com.qintess.arcadenoe.service.PetService;
 
 import lombok.AllArgsConstructor;
@@ -21,7 +26,7 @@ import lombok.AllArgsConstructor;
 @CrossOrigin(origins = {
 		"http://localhost:4200", 
 		"https://arcadenoecliente.herokuapp.com"
-		})
+})
 @RestController
 @RequestMapping("/api/v1/pet")
 //@AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -34,17 +39,39 @@ public class PetController {
 		this.petService = petService;
 	}
 
-//	@PostMapping
-//	@ResponseStatus(HttpStatus.CREATED)
-//	public MessageResponseDTO create(@RequestBody @Valid PetDTO petDTO) {
-//		return petService.create(petDTO);
-//	}
-	
+	//	@PostMapping
+	//	@ResponseStatus(HttpStatus.CREATED)
+	//	public MessageResponseDTO create2(@RequestBody @Valid PetDTO petDTO) {
+	//		return petService.create(petDTO);
+	//	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO create(@RequestBody Pet pet) {
 		System.out.println("Vamos criar pet: " + pet);
 		return petService.create(pet);
 	}
-	
+
+	//	@GetMapping
+	//	public List<PetDTO> listAll() {
+	//		return petService.listAll2();
+	//	}
+
+	@GetMapping
+	public List<Pet> listAll() {
+		return petService.listAll();
+	}
+
+
+	//	@GetMapping("/{id}")
+	//	public PetDTO findById(@PathVariable Long id) {
+	//		return petService.findById2(id);
+	//	}
+
+	@GetMapping("/{id}")
+	public Pet findById(@PathVariable Long id) throws PetNotFoundException {
+		return petService.findById(id);
+	}
+
+
 }
--- src/main/java/com/qintess/arcadenoe/service/PetService.java
+++ src/main/java/com/qintess/arcadenoe/service/PetService.java
@@ -1,5 +1,9 @@
 package com.qintess.arcadenoe.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +12,7 @@ import com.qintess.arcadenoe.dto.mapper.PetMapper;
 import com.qintess.arcadenoe.dto.request.PetDTO;
 import com.qintess.arcadenoe.dto.response.MessageResponseDTO;
 import com.qintess.arcadenoe.entity.Pet;
+import com.qintess.arcadenoe.exception.PetNotFoundException;
 import com.qintess.arcadenoe.repository.PetRepository;
 
 import lombok.AllArgsConstructor;
@@ -17,33 +22,62 @@ import lombok.AllArgsConstructor;
 public class PetService {
 
 	private final PetRepository petRepository;
-	
-//	private final PetMapper petMapper;// = PetMapper.INSTANCE;
+
+	//	private final PetMapper petMapper;// = PetMapper.INSTANCE;
 
 	@Autowired
 	public PetService(PetRepository petRepository) {
 		this.petRepository = petRepository;
 	}
-	
-//	public MessageResponseDTO create(PetDTO petDTO) {
-//		Pet petToSave = petMapper.toModel(petDTO);
-//		
-//		Pet pet = petRepository.save(petToSave);
-//		
-//		return MessageResponseDTO
-//				.builder()
-//				.message("Pet criado com ID " + pet.getId() + ".")
-//				.build();
-//	}
-	
+
+	//	public MessageResponseDTO create2(PetDTO petDTO) {
+	//		Pet petToSave = petMapper.toModel(petDTO);
+	//		
+	//		Pet pet = petRepository.save(petToSave);
+	//		
+	//		return MessageResponseDTO
+	//				.builder()
+	//				.message("Pet criado com ID " + pet.getId() + ".")
+	//				.build();
+	//	}
+
 	public MessageResponseDTO create(Pet peta) {
-		
+
 		Pet pet = petRepository.save(peta);
-		
+
 		return MessageResponseDTO
 				.builder()
 				.message("Pet criado com ID " + pet.getId() + ".")
 				.build();
 	}
-	
+
+
+	//	public List<PetDTO> listAll2() {
+	//		List<Pet> allPet = petRepository.findAll();
+	//		return allPet
+	//				.stream()
+	//				.map(petMapper::toDTO)
+	//				.collect(Collectors.toList())
+	//		;
+	//	}
+
+	public List<Pet> listAll() {
+		List<Pet> allPet = petRepository.findAll();
+		return allPet;
+	}
+
+	//	public Pet findById2(Long id) {
+	//		Optional<Pet> optionalPet = petRepository.findById(id);
+	//		return petMapper.toDTO( optionalPet.get());
+	//	}
+
+	public Pet findById(Long id) throws PetNotFoundException {
+		Optional<Pet> optionalPet = petRepository.findById(id);
+		
+		if (optionalPet.isEmpty()) {
+			throw new PetNotFoundException(id);			
+		}
+		return optionalPet.get();
+	}
+
 }
