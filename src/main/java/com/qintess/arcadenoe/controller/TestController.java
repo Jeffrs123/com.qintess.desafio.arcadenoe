d1ac8d9c411d3680e0362499aae83400eee968b2
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-18 14:19:00 -0300

    att app

--- src/main/java/com/qintess/arcadenoe/controller/TestController.java
+++ src/main/java/com/qintess/arcadenoe/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.qintess.arcadenoe.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {
+		"http://localhost:4200", 
+		"https://arcadenoecliente.herokuapp.com"
+})
 @RestController
 @RequestMapping("/api/v1/teste")
 public class TestController {
